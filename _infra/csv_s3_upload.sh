0cefe699771fa17169b271cb93b348304ed9e53b
SunriseLong <sunrise.long@yahoo.ca>
2023-10-05 13:40:46 -0400

    upload script: parameterize node_id and base_dir

--- _infra/csv_s3_upload.sh
+++ _infra/csv_s3_upload.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
+# Check for required arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <node_id> <base_dir>"
+    exit 1
+fi
+
+node_id="$1"
+base_dir="$2"
+
+# Ensure the path is absolute
+if [[ ! "$base_dir" = /* ]]; then
+    echo "please provide absolute base_dir"
+    exit 1
+fi
+
 # Current UTC hour
 current_hour=$(date -u +"%H")
 
-# Base directory
-base_dir=~/data
+# S3 base path
+s3_base_path="s3://indigo-snowflake-staging/offchain/network_feed/$node_id"
 
-# S3 path
-s3_path="s3://indigo/offchain"
-
-# Navigate through each 1-level subdirectory of ~/data/
+# Navigate through each 1-level subdirectory of the provided base_dir
 for dir in "$base_dir"/*; do
     if [ -d "$dir" ]; then
-        # Move to directory
-        cd "$dir"
+        # Extract the directory name for the S3 path
+        directory=$(basename "$dir")
+
+        # S3 upload path
+        s3_path="$s3_base_path/$directory"
 
         # Find CSVs, but exclude ones matching current UTC hour
-        find . -maxdepth 1 -type f -name '*.csv' ! -name "*-${current_hour}.csv" | while read csv_file; do
-            # Compress the CSV into ZIP
-            zip "${csv_file}.zip" "$csv_file"
+        find "$dir" -maxdepth 1 -type f -name '*.csv' ! -name "*-${current_hour}.csv" | while read csv_file; do
+            # Compress the CSV into GZIP
+            gzip "$csv_file"
 
             # Upload to S3
-            if aws s3 cp "${csv_file}.zip" "$s3_path/"; then
-                # Remove local zip and original CSV file after successful upload
-                rm "${csv_file}.zip" "$csv_file"
+            if aws s3 cp "${csv_file}.gz" "$s3_path/"; then
+                # Remove local gzip file after successful upload
+                rm "${csv_file}.gz"
             else
-                echo "Failed to upload ${csv_file}.zip to S3. Keeping the local files."
+                echo "Failed to upload ${csv_file}.gz to S3. Keeping the local files."
             fi
         done
-
-        # Return to base directory
-        cd "$base_dir"
     fi
 done

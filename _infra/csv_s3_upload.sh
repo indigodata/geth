8998ab125bbb4617057922c36232ed812b958dfd
SunriseLong <sunrise.long@yahoo.ca>
2023-10-13 16:17:08 -0400

    csv upload script look in networkfeeds

--- _infra/csv_s3_upload.sh
+++ _infra/csv_s3_upload.sh
@@ -18,32 +18,24 @@ fi
 # S3 base path
 s3_base_path="s3://indigo-snowflake-staging/offchain/network_feed/$node_id"
 
-# Navigate through each 1-level subdirectory of the provided base_dir
-for dir in "$base_dir"/*; do
-    if [ -d "$dir" ]; then
-        # Extract the directory name for the S3 path
-        directory=$(basename "$dir")
+# Current UTC hour
+current_hour=$(date -u +"%H")
 
-        # S3 upload path
-        s3_path="$s3_base_path/$directory"
-        
-        # Current UTC hour
-        current_hour=$(date -u +"%H")
+# Find CSVs in the base_dir, but exclude ones matching current UTC hour
+find "$base_dir" -maxdepth 1 -type f -name '*.csv' ! -name "*-${current_hour}.csv" | while read -r csv_file; do
+    # Compress the CSV into GZIP
+    gzip "$csv_file"
 
-        # Find CSVs, but exclude ones matching current UTC hour
-        find "$dir" -maxdepth 1 -type f -name '*.csv' ! -name "*-${current_hour}.csv" | while read -r csv_file; do
-            # Compress the CSV into GZIP
-            gzip "$csv_file"
+    # S3 upload path (no longer contains the $directory variable)
+    s3_path="$s3_base_path"
 
-            # Upload to S3
-            if aws s3 cp "${csv_file}.gz" "$s3_path/"; then
-                # Print the uploaded file's path
-                echo "Uploaded ${csv_file}.gz to $s3_path/"
-                # Remove local gzip file after successful upload
-                rm "${csv_file}.gz"
-            else
-                echo "Failed to upload ${csv_file}.gz to S3. Keeping the local files."
-            fi
-        done
+    # Upload to S3
+    if aws s3 cp "${csv_file}.gz" "$s3_path/"; then
+        # Print the uploaded file's path
+        echo "Uploaded ${csv_file}.gz to $s3_path/"
+        # Remove local gzip file after successful upload
+        rm "${csv_file}.gz"
+    else
+        echo "Failed to upload ${csv_file}.gz to S3. Keeping the local files."
     fi
 done

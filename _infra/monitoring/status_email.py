8dcdc7293f76025dfd08074230b7a263ca7274b7
SunriseLong <sunrise.long@yahoo.ca>
2024-04-29 16:21:08 -0400

    preserve Monospace

--- _infra/monitoring/status_email.py
+++ _infra/monitoring/status_email.py
@@ -1,25 +1,26 @@
 import os
 import smtplib
 
+from typing import List
 from email.message import EmailMessage
 from prettytable import PrettyTable
 
 from snowflake_service import SnowflakeDB
 from environment import SNOWFLAKE_CONFIG, EMAIL_FROM, EMAIL_PASSWORD
 
-def query_node_metrics():
+def query_node_metrics() -> List[Tuple[Any, ...]]:
     MetricDB = SnowflakeDB(SNOWFLAKE_CONFIG)
     query = """
-    select
-      NODE_ID
-    , count_if(MSG_TIMESTAMP > sysdate() - interval '1 day') as current_msg_ct
-    , count_if(MSG_TIMESTAMP between sysdate() - interval '2 day'
-        and sysdate() - interval '1 day') as previous_msg_ct
-    , (current_msg_ct - previous_msg_ct) / previous_msg_ct * 100 as pct_difference
-    from KEYSTONE_OFFCHAIN.NETWORK_FEED
-    where MSG_TIMESTAMP >= sysdate() - interval '2 day'
-    group by 1
-    order by 1
+    SELECT
+          node_id
+        , COUNT_IF(msg_timestamp > SYSDATE() - INTERVAL '1 day')        AS current_msg_ct
+        , COUNT_IF(msg_timestamp BETWEEN SYSDATE() - INTERVAL '2 day'
+            AND SYSDATE() - INTERVAL '1 day')                           AS previous_msg_ct
+        , (current_msg_ct - previous_msg_ct) / previous_msg_ct * 100    AS pct_difference
+    FROM keystone_offchain.network_feed
+    WHERE msg_timestamp >= SYSDATE() - INTERVAL '2 day'
+    GROUP BY 1
+    ORDER BY 1
     """
 
     node_metrics = MetricDB.query(query)
@@ -34,18 +35,25 @@ def format_snowflake_response(snowflake_response) -> PrettyTable:
     
     return table
 
-def send_email():
+def send_email() -> None:
     node_metrics = query_node_metrics()
     metric_table = format_snowflake_response(node_metrics)
-    print(metric_table)
-    print(metric_table.get_formatted_string('html'))
+    
+    html_table = f"""
+    <html>
+    <body>
+    <pre>
+    {metric_table.get_string()}
+    </pre>
+    </body>
+    </html>
+    """
 
     msg = EmailMessage()
     msg['From'] = EMAIL_FROM
     msg['To'] = EMAIL_FROM
     msg['Subject'] = 'Test From Local Python'
-    msg.set_content(metric_table.get_formatted_string('html'), subtype='html')
-
+    msg.set_content(html_table, subtype='html')
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_FROM, EMAIL_PASSWORD)
         smtp.send_message(msg)

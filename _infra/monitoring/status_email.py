dd6e588c8fa8e2ed9ad37cdf8150c535c4056503
SunriseLong <sunrise.long@yahoo.ca>
2024-04-29 14:18:44 -0400

    send email

--- _infra/monitoring/status_email.py
+++ _infra/monitoring/status_email.py
@@ -1,17 +1,53 @@
 import os
 import smtplib
+
 from email.message import EmailMessage
+from prettytable import PrettyTable
 
-email_from = os.environ['FROM']
-email_to = os.environ['FROM']
-email_pwd = os.environ['PASSWORD']
+from snowflake_service import SnowflakeDB
+from environment import SNOWFLAKE_CONFIG, EMAIL_FROM, EMAIL_PASSWORD
 
-msg = EmailMessage()
-msg['From'] = email_from
-msg['To'] = email_to
-msg['Subject'] = 'Test From Local Python'
-msg.set_content('This is a test email sent from a Python script.')
+def query_node_metrics():
+    MetricDB = SnowflakeDB(SNOWFLAKE_CONFIG)
+    query = """
+    select
+      NODE_ID
+    , count_if(MSG_TIMESTAMP > sysdate() - interval '1 day') as current_msg_ct
+    , count_if(MSG_TIMESTAMP between sysdate() - interval '2 day'
+        and sysdate() - interval '1 day') as previous_msg_ct
+    , (current_msg_ct - previous_msg_ct) / previous_msg_ct * 100 as pct_difference
+    from KEYSTONE_OFFCHAIN.NETWORK_FEED
+    where MSG_TIMESTAMP >= sysdate() - interval '2 day'
+    group by 1
+    order by 1
+    """
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(email_from, email_pwd)
-    smtp.send_message(msg)
+    node_metrics = MetricDB.query(query)
+    return node_metrics
+
+def format_snowflake_response(snowflake_response) -> PrettyTable:
+    table = PrettyTable()
+    table.field_names = ["Node ID", "Current Msg Ct", "Previous Msg Ct", "Percent Difference"]
+
+    for tuple_row in snowflake_response:
+        table.add_row(list(tuple_row))
+    
+    return table
+
+def send_email():
+    node_metrics = query_node_metrics()
+    metric_table = format_snowflake_response(node_metrics)
+    print(metric_table)
+    print(metric_table.get_formatted_string('html'))
+
+    msg = EmailMessage()
+    msg['From'] = EMAIL_FROM
+    msg['To'] = EMAIL_FROM
+    msg['Subject'] = 'Test From Local Python'
+    msg.set_content(metric_table.get_formatted_string('html'), subtype='html')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_FROM, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+send_email()

0dcd82300c17fd913b6507ca0997c4b063147c32
SunriseLong <sunrise.long@yahoo.ca>
2024-04-29 14:17:42 -0400

    enviroment

---
+++ _infra/monitoring/environment.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(".env"), # load variables from .env file
+    **os.environ,  # override loaded values with environment variables
+}
+
+EMAIL_FROM = config.get('EMAIL_FROM')
+EMAIL_PASSWORD = config.get('EMAIL_PASSWORD')
+
+SNOWFLAKE_CONFIG = {
+    'user': config.get('SNOWFLAKE_USER'),
+    'password': config.get('SNOWFLAKE_PASSWORD'),
+    'account': config.get('SNOWFLAKE_ACCOUNT'),
+    'role': config.get('SNOWFLAKE_ROLE'),
+    'warehouse': config.get('SNOWFLAKE_WAREHOUSE'),
+    'database': config.get('SNOWFLAKE_DATABASE'),
+    'schema': config.get('SNOWFLAKE_SCHEMA'),
+}
+
+SENTRY_DSN = config.get("SENTRY_DSN", "")
